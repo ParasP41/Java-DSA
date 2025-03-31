16e3479dcc7c3480a919dfa56fabe852c8326063
ParasP41 <parasvishwakarma198@gmail.com>
2025-03-31 19:52:32 +0530

    java_dsa

---
+++ Practice/src/com/Paras/Primitive.java
@@ -0,0 +1,13 @@
+package com.Paras;
+
+import java.util.Scanner;
+
+public class Primitive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       int a=input.nextInt();
+       int b=input.nextInt();
+       int c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ Practice/src/com/Paras/Type.java
@@ -0,0 +1,33 @@
+package com.Paras;
+
+import java.util.Scanner;
+
+public class Type {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the value of num");
+//        float num=input.nextFloat();  //type conversion
+//        System.out.println(num);
+
+//        int num2 = (int) (56.89f); //type conversion
+//        System.out.println(num2);
+
+//        automatic type promotion in expression
+//        int a=257;
+//        byte b=(byte)(a); //257%256=1
+//        System.out.println(b);
+
+//        byte a=40;
+//        byte b=60;
+//        byte c=100;
+////        int d=(a*b/c);
+//        byte d=(byte)(a*b/c); //because when we are performing the operation on the byte it is automatically converting into the int
+//        System.out.println(d);
+
+//        int number='A';
+//        System.out.println(number);
+        //java follow the unicode principle
+
+
+    }
+}
