2df57326a261e66e27c83cbc7876138d37df95b7
ParasP41 <parasvishwakarma198@gmail.com>
2025-04-21 22:58:19 +0530

    Java+dsa

--- Assignments/src/Assignment05/Assi05.java
+++ Assignments/src/Assignment05/Assi05.java
@@ -1,80 +1,27 @@
 package Assignment05;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Assi05 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-//        String str = "abcdefgh";
-//        String check = "cde";
-//        int target = 0;
-//        char[] arr = str.toCharArray();
-//        for (int i = 0; i < check.length(); i++) {
-//        int start = 0;
-//        int end = arr.length - 1;
-//        while (start <= end) {
-//                target = (int) ((char) check.charAt(i));
-//                int mid = start + (end - start) / 2;
-//                if (target > (int) (arr[mid])) {
-//                    start = mid + 1;
-//                } else if (target < (int) (arr[mid])) {
-//                    end = mid - 1;
-//                } else if (target == (int) (arr[mid])) {
-//                    System.out.println(mid);
-//                    return;
-//                }
-//            }
-//        }
-
-//        String str = "feet";
-//        System.out.println(checkPangram(str));
-
-
-//
-//    static boolean checkPangram(String sentence) {
-//        char[] abc = new char[26];
-//        for (int i = 0; i < abc.length; i++) {
-//            char alpha = (char) ('a' + i);
-//            abc[i] = alpha;
-//        }
-//        for (int i = 0; i < abc.length; i++) {
-//            for (int j = 0; j < abc.length; j++) {
-//                if (sentence.charAt(i) == abc[j]) {
-//                    return true;
-//                } else if (sentence.length() < 26) {
-//                    return false;
-//                }
-//            }
-//        }
-//        return false;
-
-        int sum=0;
-        int[] arr={9,8,7,6,5,4,3,2,1,0};
-        int count=0;
-
+        int[] arr={1,2,3,1,2,3};
+        int y=0;
+        int z=0;
+        int[] newArr=new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            sum=(sum*10)+(arr[i]);
+            if(i%2==0)
+            {
+                newArr[i]=arr[z];
+                z++;
+            }else
+            {
+                int x= (arr.length)/2;
+                newArr[i] = arr[x + y];
+                y++;
+            }
         }
-        sum+=1;
-        int y=sum;
-        while(y>0)
-        {
-            y/=10;
-            count++;
-        }
-//        System.out.println(count);
-        int[] up=new int[count];
-        for (int i = up.length-1; i >= 0 ; i--) {
-            int x=sum;
-            x=x%10;
-            up[i]=x;
-            sum/=10;
-        }
-        System.out.println(Arrays.toString(up));
-//        System.out.println(sum);
-
+        System.out.println(Arrays.toString(newArr));
     }
 
 }
