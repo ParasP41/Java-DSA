4b634eba6bd4243c6209d7da05e3ebb153a296a8
ParasP41 <parasvishwakarma198@gmail.com>
2025-04-29 23:25:43 +0530

    java+dsa

--- Assignments/src/Assignment05/Assi05.java
+++ Assignments/src/Assignment05/Assi05.java
@@ -4,6 +4,30 @@ import java.util.*;
 
 class Assi05 {
     public static void main(String[] args) {
+        int[][] arr={
+                {1,2},
 
+        };
+        int r=1,c=1;
+        ArrayList<Integer> list=new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                list.add(arr[i][j]);
+            }
+        }
+        int[][] newArr=new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int x=c*i+j;
+                if(x<list.size())
+                {
+                    newArr[i][j]=list.get(x);
+                }else{
+                    return ;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(newArr));
     }
 }
+
