b3fec44b61f1050644ad690d704c32debe7c9813
ParasP41 <parasvishwakarma198@gmail.com>
2025-04-30 15:38:16 +0530

    java+dsa

--- Assignments/src/Assignment05/Assi05.java
+++ Assignments/src/Assignment05/Assi05.java
@@ -1,33 +1,28 @@
 package Assignment05;
 
+import java.sql.Array;
 import java.util.*;
 
 class Assi05 {
     public static void main(String[] args) {
-        int[][] arr={
-                {1,2},
-
-        };
-        int r=1,c=1;
-        ArrayList<Integer> list=new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                list.add(arr[i][j]);
-            }
-        }
-        int[][] newArr=new int[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                int x=c*i+j;
-                if(x<list.size())
-                {
-                    newArr[i][j]=list.get(x);
-                }else{
-                    return ;
-                }
-            }
-        }
-        System.out.println(Arrays.deepToString(newArr));
+        System.out.println(Arrays.toString(sumZero(5)));
     }
+    static int[] sumZero(int n)
+    {
+        int[] arr=new int[n];
+        for (int i = 0,j=n-1; i <= j; i++,j--) {
+            if(i==j)
+            {
+                arr[i]=0;
+            }else
+            {
+                arr[i]=j;
+                arr[j]=-j;
+            }
+        }
+        return arr;
+    }
+
+
 }
 
