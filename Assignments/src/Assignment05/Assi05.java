79d4e187c61a916cd912026b424a63404ee4bfb0
ParasP41 <parasvishwakarma198@gmail.com>
2025-05-09 19:40:22 +0530

    DSA

--- Assignments/src/Assignment05/Assi05.java
+++ Assignments/src/Assignment05/Assi05.java
@@ -1,39 +1,32 @@
 package Assignment05;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 class Assi05 {
     public static void main(String[] args) {
-        int[][] arr={
-                {1,1},
-                {0,0}
-        };
-        mat(arr,2,3);
-    }
-    static void mat(int[][] arr,int m,int n)
-    {
-        int[][] newArr=new int[m][n];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if(j%2==0){
-                    int x=arr[i][j];
-                    for (int k = 0; k < n; k++) {
-                        newArr[x][k]+=1;
-                    }
-                }
-                else{
-                    int x=arr[i][j];
-                    for (int k = 0; k < m; k++) {
-                        newArr[k][x]+=1;
-                    }
-                }
-            }
-        }
-        System.out.println(Arrays.deepToString(newArr));
+        int[] arr = {1, 2, 3, 3, 3, 3, 4};
+        System.out.println(Arrays.toString(spiral(arr,3)));
     }
 
+    static int[] spiral(int[] arr,int target) {
+        int[] newArr={-1,-1};
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==target)
+            {
+                newArr[0]=i;
+                break;
+            }
+        }
+        for (int i = arr.length-1; i >= 0 ; i--) {
+            if(arr[i]==target)
+            {
+                newArr[1]=i;
+                break;
+            }
+        }
+        return newArr;
+    }
 }
 
 
