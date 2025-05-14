4f816715c3e96252a44da8b0c5e78ad0762faeeb
ParasP41 <parasvishwakarma198@gmail.com>
2025-05-14 07:24:58 +0530

    dsa

--- Assignments/src/Assignment05/Assi05.java
+++ Assignments/src/Assignment05/Assi05.java
@@ -1,18 +1,45 @@
 package Assignment05;
 
-import java.util.ArrayList;
+
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 class Assi05 {
     public static void main(String[] args) {
-        int[] arr = {0, 2, 1};
-        spiral(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {1,2,3,4};
+        rotate(arr);
     }
 
-    static void spiral(int[] arr) {
+    static void rotate(int[] arr) {
+        int[] newArr = new int[arr.length];
+        int sum = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                continue;
+            } else {
+                sum *= arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+               int[] zeroArr=new int[arr.length];
+               zeroArr[i]=sum;
+                System.out.println(Arrays.toString(zeroArr));
+                return;
+            } else {
+                newArr[i] = sum / arr[i];
+            }
+        }
+        System.out.println(Arrays.toString(newArr));
+
 
     }
+
+//    static void swap(int[] arr, int i, int j) {
+//        int temp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = temp;
+//    }
 
 }
 
