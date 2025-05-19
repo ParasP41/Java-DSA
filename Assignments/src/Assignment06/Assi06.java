bd4860f9db5f86cc6dc9c57b02d15caba3a9cbaa
ParasP41 <parasvishwakarma198@gmail.com>
2025-05-19 19:03:11 +0530

    java+dsa

--- Assignments/src/Assignment06/Assi06.java
+++ Assignments/src/Assignment06/Assi06.java
@@ -5,33 +5,52 @@ import java.util.Arrays;
 
 public class Assi06 {
     public static void main(String[] args) {
-        int[] arr = {3, 5};
-        System.out.println(sum(arr));
-
+        int[] arr = {3,3,7,7,10,11,11};
+        System.out.println(find(arr));
     }
 
-    static int sum(int[] arr) {
-
-
+    public static int find(int[] arr) {
+        for (int i = 0; i < arr.length; ) {
+            int search = search(arr, arr[i]);
+            if (search == 0) {
+                if (arr[search] == arr[search + 1]) {
+                    i++;
+                }else {
+                    return arr[search];
+                }
+            }
+            if (search == arr.length - 1) {
+                if (arr[search] == arr[search - 1]) {
+                    i++;
+                }else {
+                    return arr[search];
+                }
+            }
+            if (arr[search] == arr[search + 1] || arr[search] == arr[search - 1]) {
+                i++;
+            }else {
+                return arr[search];
+            }
+        }
+        return -1;
     }
 
-
-//    public static int search(int[] arr, int target) {
-//        int start = 0;
-//        int end = arr.length - 1;
-//        while (start <= end) {
-//            int mid = start + (end - start) / 2;
-//            ;
-//            if (target == arr[mid]) {
-//                return mid;
-//            } else if (target > arr[mid]) {
-//                start = mid + 1;
-//            } else {
-//                end = mid - 1;
-//            }
-//        }
-//        return -1;
-//    }
+    public static int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            ;
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 
 
 }
