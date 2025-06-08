d4aa220da1f204d03eb41643e55ca77ebe8a0604
ParasP41 <parasvishwakarma198@gmail.com>
2025-06-08 22:39:24 +0530

    java+dsa

--- Assignments/src/Assignment05/Assi05.java
+++ Assignments/src/Assignment05/Assi05.java
@@ -6,29 +6,30 @@ import java.util.Arrays;
 
 class Assi05 {
     public static void main(String[] args) {
-        int[] arr={1,2,2,3,3};
-        System.out.println(thirdMax(arr));
+        int[] arr = {4000, 3000, 1000, 2000};
+        System.out.println(average(arr));
     }
 
-    public static int thirdMax(int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
+    public static double average(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            list.add(arr[i]);
-        }
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i - 1] == arr[i]) {
-                list.remove(arr[i]);
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+            if (max < arr[i]) {
+                max = arr[i];
             }
         }
-        if(list.size()==1)
-        {
-            return list.get(0);
-        }else if(list.size()==2){
-            return list.get(1);
+        int x= arr.length-2;
+        double total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
         }
-        return list.get(2);
+        total = total - (min + max);
+        double avg = total / x;
+        return avg;
     }
-
 
 
 //    static void swap(int[] arr, int i, int j) {
