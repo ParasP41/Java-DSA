07aeaa9cc4ff4a619a5880b6907b43090a784ddc
ParasP41 <parasvishwakarma198@gmail.com>
2025-06-25 16:39:08 +0530

    dsa

--- Assignments/src/Assignment08/Assi08.java
+++ Assignments/src/Assignment08/Assi08.java
@@ -4,25 +4,32 @@ import java.sql.Array;
 import java.util.Arrays;
 
 public class Assi08 {
+
     public static void main(String[] args) {
-        System.out.println(mergeAlternately("abc","abc"));
+        System.out.println(sdeep());
     }
-
-    public static String mergeAlternately(String word1, String word2) {
-        String str = "";
-        for (int i = 0; i < Math.min(word1.length(), word2.length()); i++) {
-            str += word1.charAt(i);
-            str += word2.charAt(i);
+    static boolean sdeep(String a,String b)
+    {
+        for(int i=0,j=a.length()-1;i<j;i++,j--)
+        {
+            if(a.charAt(i)!=b.charAt(j))
+            {
+                return false;
+            }
         }
-
-        if (word1.length() > word2.length()) {
-            str += word1.substring(word2.length());
+        return true;
+    }
+    static boolean pdeep(String a,String b)
+    {
+        for(int i=0,j=a.length()-1;i<j;i++,j--)
+        {
+            if(b.charAt(i)!=a.charAt(j))
+            {
+                return false;
+            }
         }
-
-        if (word1.length() < word2.length()) {
-            str += word2.substring(word1.length());
-        }
-
-        return str;
+        return true;
     }
 }
+
+
